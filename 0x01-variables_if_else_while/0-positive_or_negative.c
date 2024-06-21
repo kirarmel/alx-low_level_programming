bf219d7cd68136e0841db78f7f38cda261635d9d
Kira Armel Kayisire <kirarmel@gmail.com>
2024-06-21 07:09:06 -0700

    added a new file

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>   
+
+int main() {
+    int n;
+
+    srand(time(NULL));
+
+    n = rand() % 201 - 100; 
+
+    if (n > 0) {
+        printf("%d is positive\n", n);
+    } else if (n < 0) {
+        printf("%d is negative\n", n);
+    } else {
+        printf("%d is zero\n", n);
+    }
+
+    return (0);
+}
