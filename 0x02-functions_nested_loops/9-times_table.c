61b2ad57de1bf5f3a374f66dfc872328c8c29cf2
Kira Armel Kayisire <kirarmel@gmail.com>
2024-06-26 15:38:02 -0700

    modified files

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,35 +5,35 @@
  */
 void times_table(void)
 {
-    int row, column, product;
+	int row, column, product;
 
-    for (row = 0; row <= 9; row++)
-    {
-        for (column = 0; column <= 9; column++)
-        {
-            product = row * column;
+	for (row = 0; row <= 9; row++)
+	{
+		for (column = 0; column <= 9; column++)
+		{
+			product = row * column;
 
-            if (column == 0)
-            {
-                _putchar('0');
-            }
-            else
-            {
-                _putchar(',');
-                _putchar(' ');
+			if (column == 0)
+			{
+				_putchar('0');
+			}
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
 
-                if (product >= 10)
-                {
-                    _putchar((product / 10) + '0');
-                    _putchar((product % 10) + '0');
-                }
-                else
-                {
-                    _putchar(' ');
-                    _putchar(product + '0');
-                }
-            }
-        }
-        _putchar('\n');
-    }
+				if (product >= 10)
+				{
+					_putchar((product / 10) + '0');
+					_putchar((product % 10) + '0');
+				}
+				else
+				{
+					_putchar(' ');
+					_putchar(product + '0');
+				}
+			}
+		}
+		_putchar('\n');
+	}
 }
