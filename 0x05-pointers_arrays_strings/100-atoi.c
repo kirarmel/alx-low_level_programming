358cd9fff01f150fb86f26aa3f15ac000ae7fefb
Kira Armel Kayisire <kirarmel@gmail.com>
2024-07-01 16:50:11 -0700

    added new files

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * _atoi - converts a string to an integer
+ * @s: the string to convert
+ *
+ * Return: the integer value of the string
+ */
+int _atoi(char *s)
+{
+    int i = 0;
+    int sign = 1;
+    int result = 0;
+
+ 
+    while (s[i] != '\0' && (s[i] < '0' || s[i] > '9'))
+    {
+        
+        if (s[i] == '-')
+        {
+            sign *= -1;
+        }
+        else if (s[i] == '+')
+        {
+ 
+        }
+        i++;
+    }
+    
+    while (s[i] != '\0' && (s[i] >= '0' && s[i] <= '9'))
+    {
+        result = result * 10 + (s[i] - '0');
+        i++;
+    }
+
+    return result * sign;
+}
---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define PASSWORD_LENGTH 8
+
+/**
+ * main - generates a random password for the 101-crackme program
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+    char password[PASSWORD_LENGTH + 1];
+    int i;
+
+    
+    srand(time(NULL));
+
+   
+    for (i = 0; i < PASSWORD_LENGTH; i++)
+    {
+        password[i] = rand() % 94 + 33; 
+    }
+    password[PASSWORD_LENGTH] = '\0'; 
+
+    printf("%s", password);
+
+    return 0;
+}
