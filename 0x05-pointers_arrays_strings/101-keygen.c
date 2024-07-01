ca12c918f31eac8c3a96fb3e24a51d52e2f9a51d
Kira Armel Kayisire <kirarmel@gmail.com>
2024-07-01 16:53:59 -0700

    modified a file

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <time.h>
 
-#define PASSWORD_LENGTH 8
+#define PASSWORD_LENGTH 15
 
 /**
  * main - generates a random password for the 101-crackme program
@@ -14,15 +14,13 @@ int main(void)
     char password[PASSWORD_LENGTH + 1];
     int i;
 
-    
     srand(time(NULL));
 
-   
     for (i = 0; i < PASSWORD_LENGTH; i++)
     {
         password[i] = rand() % 94 + 33; 
     }
-    password[PASSWORD_LENGTH] = '\0'; 
+    password[PASSWORD_LENGTH] = '\0' ;
 
     printf("%s", password);
 
