832177a51794b1d92386183bf06c1c4b022bb344
Kira Armel Kayisire <kirarmel@gmail.com>
2024-08-08 08:11:39 -0700

    added new files

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int num = 0;
+	int i;
+
+	if (!b)
+	return (0);
+	
+	for (i = 0; b[i] != '\0'; i++)
+	{
+	if (b[i] != '0' && b[i] != '1')
+	return (0);
+
+	num = num * 2 + (b[i] - '0');
+	}
+
+	return (num);
+}
